1e4c8b01cd066e8bbb57c94f019c1b64f049b9ba
shashwat46 <shashwat.singh0406@gmail.com>
2024-09-02 12:30:40 +0530

    solved wrongSubtraction.cpp

--- A/wrongSubtraction.cpp
+++ A/wrongSubtraction.cpp
@@ -6,6 +6,18 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int n, k;
+    cin >> n >> k;
+
+    while(k--){
+        if(n%10 == 0){
+            n = n/10;
+        }
+        else n -= 1;
+    }
+
+    cout<< n;
+
+    return 0;
 
 }
