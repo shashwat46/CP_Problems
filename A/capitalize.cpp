28fe1bd70497b6111d9cf3330df7056549d7188d
shashwat46 <shashwat.singh0406@gmail.com>
2024-09-04 15:22:50 +0530

    solved Word Capitalization Problem

---
+++ A/capitalize.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string input; 
+    cin>> input;
+
+    if(input[0] > 91)
+    input[0] = input[0] - 32;
+
+    cout << input;
+
+    return 0;
+}
